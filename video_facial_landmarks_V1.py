dbba2de3582ac320b2949582aa257f59cfede026
hossein hayati <hossein.hayati128@gmail.com>
2018-09-13 21:27:43 +0430

    mistake

--- video_facial_landmarks_V1.py
+++ video_facial_landmarks_V1.py
@@ -1,6 +1,6 @@
 
-author:hossein hayati
-last update : 13/9/2018
+#author:hossein hayati
+#last update : 13/9/2018
 
 # USAGE
 # python video_facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat
